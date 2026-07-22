e3780dc13b3878b2765fed9a049c405cb7858bf0
tad-iizuka <tad.iizuka@gmail.com>
2026-07-22 16:32:52 +0900

    change

--- mini_serv/mini_serv.c
+++ mini_serv/mini_serv.c
@@ -6,7 +6,7 @@
 /*   By: tiizuka <tiizuka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/01 13:01:19 by akyoshid          #+#    #+#             */
-/*   Updated: 2026/07/22 03:55:17 by tiizuka          ###   ########.fr       */
+/*   Updated: 2026/07/22 16:32:31 by tiizuka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-///////////////////////////////////////////////////
+///////////////////////ß////////////////////////////
 // Global variables
 ///////////////////////////////////////////////////
-int			g_server_fd;
-int			g_max_fd;
-fd_set		g_master_fds;
+int g_server_fd;
+int g_max_fd;
+fd_set g_master_fds;
 
-typedef struct s_database {
-	int		id;
-	char	*buff;
-}	t_database;
+typedef struct s_database
+{
+	int id;
+	char *buff;
+} t_database;
 
-t_database	g_database[FD_SETSIZE];
-int			g_next_id = 0;
+t_database g_database[FD_SETSIZE];
+int g_next_id = 0;
 
 ///////////////////////////////////////////////////
 // Provided functions
@@ -42,10 +43,10 @@ int			g_next_id = 0;
 // split buf to (new) buf & msg
 // (old) buf memory will be reused for msg (split by NULL)
 // (new)buf & msg should be freed after this function
-int	extract_message(char **buf, char **msg)
+int extract_message(char **buf, char **msg)
 {
-	char	*newbuf;
-	int		i;
+	char *newbuf;
+	int i;
 
 	*msg = 0;
 	if (*buf == 0)
@@ -75,10 +76,10 @@ int	extract_message(char **buf, char **msg)
 // add will not be freed by this function
 // buf can be NULL
 // add can't be NULL
-char	*str_join(char *buf, char *add)
+char *str_join(char *buf, char *add)
 {
-	char	*newbuf;
-	int		len;
+	char *newbuf;
+	int len;
 
 	if (buf == 0)
 		len = 0;
@@ -98,19 +99,24 @@ char	*str_join(char *buf, char *add)
 ///////////////////////////////////////////////////
 // Helper functions
 ///////////////////////////////////////////////////
-void	print_log(char *str) {
+void print_log(char *str)
+{
 	if (str != NULL)
 		write(2, str, strlen(str));
 }
 
-void	fatal_error(void) {
+void fatal_error(void)
+{
 	write(2, "Fatal error\n", 12);
 	exit(1);
 }
 
-void	clean_up(void) {
-	for (int fd = 0; fd <= g_max_fd; ++fd) {
-		if (FD_ISSET(fd, &g_master_fds) && fd != g_server_fd) {
+void clean_up(void)
+{
+	for (int fd = 0; fd <= g_max_fd; ++fd)
+	{
+		if (FD_ISSET(fd, &g_master_fds) && fd != g_server_fd)
+		{
 			FD_CLR(fd, &g_master_fds);
 			g_database[fd].id = -1;
 			free(g_database[fd].buff);
@@ -122,9 +128,11 @@ void	clean_up(void) {
 	close(g_server_fd);
 }
 
-void	broadcast(int sender_fd, const char *mes) {
+void broadcast(int sender_fd, const char *mes)
+{
 	// print_log("broadcast\n");
-	for (int fd = 0; fd <= g_max_fd; ++fd) {
+	for (int fd = 0; fd <= g_max_fd; ++fd)
+	{
 		if (FD_ISSET(fd, &g_master_fds) && fd != g_server_fd && fd != sender_fd)
 			send(fd, mes, strlen(mes), 0);
 	}
@@ -133,10 +141,12 @@ void	broadcast(int sender_fd, const char *mes) {
 ///////////////////////////////////////////////////
 // Connection
 ///////////////////////////////////////////////////
-void	handle_new_connection() {
+void handle_new_connection()
+{
 	// print_log("handle_new_connection\n");
 	int new_fd = accept(g_server_fd, NULL, NULL);
-	if (new_fd >= 0) {
+	if (new_fd >= 0)
+	{
 		// set up
 		FD_SET(new_fd, &g_master_fds);
 		if (g_max_fd < new_fd)
@@ -147,12 +157,13 @@ void	handle_new_connection() {
 		// broadcast
 		char message[64];
 		sprintf(message,
-			"server: client %d just arrived\n", g_database[new_fd].id);
+						"server: client %d just arrived\n", g_database[new_fd].id);
 		broadcast(new_fd, message);
 	}
 }
 
-void	handle_disconnection(int fd) {
+void handle_disconnection(int fd)
+{
 	// print_log("handle_disconnection\n");
 	// broadcast
 	char message[64];
@@ -169,16 +180,19 @@ void	handle_disconnection(int fd) {
 ///////////////////////////////////////////////////
 // Recv data
 ///////////////////////////////////////////////////
-void	recv_data(int recv_fd) {
+void recv_data(int recv_fd)
+{
 	// print_log("recv_data\n");
 	char data[4096];
 	int ret1 = recv(recv_fd, data, sizeof(data) - 1, 0);
-	if (ret1 > 0) {
+	if (ret1 > 0)
+	{
 		data[ret1] = '\0';
 
 		// join buff + data
 		char *joined_str = str_join(g_database[recv_fd].buff, data);
-		if (joined_str == NULL) {
+		if (joined_str == NULL)
+		{
 			clean_up();
 			fatal_error();
 		}
@@ -186,14 +200,17 @@ void	recv_data(int recv_fd) {
 
 		// extract message
 		char *message;
-		while (1) {
+		while (1)
+		{
 			int ret2 = extract_message(&g_database[recv_fd].buff, &message);
-			if (ret2 > 0) {
+			if (ret2 > 0)
+			{
 
 				char prefix[32];
 				sprintf(prefix, "client %d: ", g_database[recv_fd].id);
 				char *prefix_alloced = str_join(NULL, prefix);
-				if (prefix_alloced == NULL) {
+				if (prefix_alloced == NULL)
+				{
 					free(message);
 					clean_up();
 					fatal_error();
@@ -201,7 +218,8 @@ void	recv_data(int recv_fd) {
 
 				char *message_with_prefix = str_join(prefix_alloced, message);
 				free(message);
-				if (message_with_prefix == NULL) {
+				if (message_with_prefix == NULL)
+				{
 					free(prefix_alloced);
 					clean_up();
 					fatal_error();
@@ -209,15 +227,20 @@ void	recv_data(int recv_fd) {
 
 				broadcast(recv_fd, message_with_prefix);
 				free(message_with_prefix);
-
-			} else if (ret2 == 0) {
-				break ;
-			} else {
+			}
+			else if (ret2 == 0)
+			{
+				break;
+			}
+			else
+			{
 				clean_up();
 				fatal_error();
-			}		
+			}
 		}
-	} else if (ret1 <= 0) {
+	}
+	else if (ret1 <= 0)
+	{
 		handle_disconnection(recv_fd);
 	}
 }
@@ -225,8 +248,10 @@ void	recv_data(int recv_fd) {
 ///////////////////////////////////////////////////
 // main
 ///////////////////////////////////////////////////
-int	main(int argc, char **argv) {
-	if (argc < 2) {
+int main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
 		write(2, "Wrong number of arguments\n", 26);
 		return (1);
 	}
@@ -240,12 +265,14 @@ int	main(int argc, char **argv) {
 	addr.sin_family = AF_INET;
 	addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 	addr.sin_port = htons(atoi(argv[1]));
-	if (bind(g_server_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+	if (bind(g_server_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+	{
 		close(g_server_fd);
 		fatal_error();
 	}
 
-	if (listen(g_server_fd, 128) < 0) {
+	if (listen(g_server_fd, 128) < 0)
+	{
 		close(g_server_fd);
 		fatal_error();
 	}
@@ -254,19 +281,25 @@ int	main(int argc, char **argv) {
 	FD_SET(g_server_fd, &g_master_fds);
 	g_max_fd = g_server_fd;
 
-	while (1) {
+	while (1)
+	{
 		fd_set read_fds = g_master_fds;
 		int ret = select(g_max_fd + 1, &read_fds, NULL, NULL, NULL);
-		if (ret > 0) {
-			for (int fd = 0; fd <= g_max_fd; ++fd) {
-				if (FD_ISSET(fd, &read_fds)) {
+		if (ret > 0)
+		{
+			for (int fd = 0; fd <= g_max_fd; ++fd)
+			{
+				if (FD_ISSET(fd, &read_fds))
+				{
 					if (fd == g_server_fd)
 						handle_new_connection();
 					else
 						recv_data(fd);
 				}
 			}
-		} else if (ret < 0) {
+		}
+		else if (ret < 0)
+		{
 			if (errno == EINTR)
 				continue;
 			clean_up();
